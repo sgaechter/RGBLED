6c0553e62f5db7471dbf9de2f82544426ff5badb
sgaechter <sven@familiegaechter.ch>
2018-12-25 19:30:06 +0100

    Update RGBLED.h

--- RGBLED.h
+++ RGBLED.h
@@ -1,106 +1,132 @@
-/*
-	RGBLED.h - Library for controlling an RGB LED
-	Created by Bret Stateham, November 13, 2014
-	Released into the public domain
-	You can get the latest version from http://github.com/BretStateham/rgbled
+#include "Arduino.h"
+#include "RGBLED.h"
 
-	To create an instance, call the RGBLED constructor with the red, green, and blue pins, as well as the type, COMMON_CATHODE or COMMON_ANODE
+// Constructor
+RGBLED::RGBLED(int rPin, int gPin, int bPin, common_type type) {
+	redPin = rPin;
+	greenPin = gPin;
+	bluePin = bPin;
+	commonType = type;
 
-	There are two basic types of RGB leds, COMMON_CATHODE and COMMON_ANODE:
-	COMMON_CATHODE RGB LEDs have a common cathode "leg" that should be connected to ground (GND) arduino
-	COMMON_ANODE RGB LEDs have a common anode "leg" that should be connected to 5V on the arduino
-
-	You need to physically connect the RGB LED's Red, Green and Blue pins to Pulse Width Modulation (PWM) capable pins on the Arduino
-	On the Arduino, digital pins 3,5,6,9,10 & 11 are PWM pins as indicated by the "~" infront of their pin numbers on the board
-
-	Create an RGBLED instance by passing the red, green, blue and type (COMMON_CATHODE or COMMON_ANODE) into the constructor.
-	
-	For example, the following code would create an RGBLED instance named rgbLED for a COMMON_ANODE RGB LEDs with the red, green and blue 
-	legs connected to 11,9 & 6 respectively:
-
-	RGBLED rgbLed(11,9,6,COMMON_ANODE);
-
-	You can then call then various methods on the RGBLED instance:
-
-	writeRGB(red,green,blue);
-	writeRed(red);
-	writeGreen(green);
-	writeBlue(blue);
-	randomColor();
 	turnOff();
-	mapValue();
+}
 
-*/
+void RGBLED::writeRGB(int red, int green, int blue) {
+  writeRed(red);
+  writeGreen(green);
+  writeBlue(blue);
+}
 
-#ifndef RGBLED_h
-#define RGBLED_h
+void RGBLED::writeRed(int red) {
+	redValue = red;
+	redMappedValue = mapValue(redValue);
+	analogWrite(redPin,redMappedValue);
+}
 
-typedef enum 
-{
-  // Used to indicate the RGB LED is a Common Cathode LED
-  COMMON_CATHODE = 0,
-  // Used to indicate the RGB LED is a Common Anode LED
-  COMMON_ANODE   = 1,
-} common_type;
+void RGBLED::writeGreen(int green) {
+	greenValue = green;
+	greenMappedValue = mapValue(greenValue);
+	analogWrite(greenPin,greenMappedValue);
+}
 
-class RGBLED
-{
-public:
-	
-	//LED Pins
-	int redPin;
-	int greenPin;
-	int bluePin;
+void RGBLED::writeBlue(int blue) {
 
-	//RGB Values
-	int redValue;
-	int greenValue;
-	int blueValue;
+	blueValue = blue;
+	blueMappedValue = mapValue(blueValue);
+	analogWrite(bluePin,blueMappedValue);
+}
 
-	//LED Type: COMMON_ANODE or COMMON_CATHODE
-	common_type commonType;
+void RGBLED::turnOff() {
+	writeRed(0);
+	writeGreen(0);
+	writeBlue(0);
+}
 
-	//RGB Values mapped based on commonType:
-	int redMappedValue;
-	int greenMappedValue;
-	int blueMappedValue;
+void RGBLED::writeRandom() {
+	int r = random(0,255);
+	int g = random(0,255);
+	int b = random(0,255);
 
-	//Constructor 
-	RGBLED(int redPin,int greenPin, int bluePin, common_type commonType);
-	
-	// Single method to set all pins
-	// red,green,blue values range from 0(off)-255(on)
-	void writeRGB(int red, int green, int blue);
+	writeRGB(r,g,b);
+}
 
-	// Sets just the red pin
-	// Values range from 0(off)-255(on)
-	void writeRed(int red);
+int RGBLED::mapValue(int value) {
+	value = (value < 0) ? 0 : (value > 255) ? 255 : value;
+	value = (commonType == COMMON_ANODE) ? 255-value : value;
+	return value;
+}
 
-	// Sets just the greem pin
-	// Values range from 0(off)-255(on)
-	void writeGreen(int green);
+// Convert a given HSV (Hue Saturation Value) to RGB(Red Green Blue) and set the led to the color
+//   h is hue value, integer between 0 and 360
+//   s is saturation value, double between 0 and 1
+//   v is value, double between 0 and 1
+// Stolen from: http://eduardofv.com/read_post/179-Arduino-RGB-LED-HSV-Color-Wheel-
+// Based on: http://www.splinter.com.au/converting-hsv-to-rgb-colour-using-c/
+void RGBLED::writeHSV(int h, double s, double v) {
+  //this is the algorithm to convert from RGB to HSV
+  double r=0; 
+  double g=0; 
+  double b=0;
 
-	// Sets just the blue pin
-	// Values range from 0(off)-255(on)
-	void writeBlue(int blue);
+  double hf=h/60.0;
 
-	// Turns all pins off
-	void turnOff(void);
+  int i=(int)floor(h/60.0);
+  double f = h/60.0 - i;
+  double pv = v * (1 - s);
+  double qv = v * (1 - s*f);
+  double tv = v * (1 - s * (1 - f));
 
-	// Show a random color
-	void writeRandom(void);
+  switch (i)
+  {
+  case 0: //rojo dominante
+    r = v;
+    g = tv;
+    b = pv;
+    break;
+  case 1: //verde
+    r = qv;
+    g = v;
+    b = pv;
+    break;
+  case 2: 
+    r = pv;
+    g = v;
+    b = tv;
+    break;
+  case 3: //azul
+    r = pv;
+    g = qv;
+    b = v;
+    break;
+  case 4:
+    r = tv;
+    g = pv;
+    b = v;
+    break;
+  case 5: //rojo
+    r = v;
+    g = pv;
+    b = qv;
+    break;
+  }
 
-	// Writes the LED based on HSV values
-	void writeHSV(int h, double s, double v);
+  //set each component to a integer value between 0 and 255
+  int red=constrain((int)255*r,0,255);
+  int green=constrain((int)255*g,0,255);
+  int blue=constrain((int)255*b,0,255);
 
-	// Show the HSV Color Wheel
-	// Take in the delay (dly) in milliseconds between each color
-	void writeColorWheel(int dly);
-
-	// Map a value based on the common_type
-	int mapValue(int value);
+  writeRGB(red,green,blue);
+}
 
 
-};
-
-#endif
+// Cycle through the color wheel
+// Stolen from: http://eduardofv.com/read_post/179-Arduino-RGB-LED-HSV-Color-Wheel-
+void RGBLED::writeColorWheel(int dly, int repeat) {
+  //The Hue value will vary from 0 to 360, which represents degrees in the color wheel
+	for(int cycle=0;cycle<repeat;cycle++){
+		for(int hue=0;hue<360;hue++){
+			writeHSV(hue,1,1); //We are using Saturation and Value constant at 1
+			delay(dly); //each color will be shown for 10 milliseconds
+		}
+	}
+}
